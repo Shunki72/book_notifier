83a08c4cee785ec80238a197b66bcfa5282df385
Shunki72 <144867611+Shunki72@users.noreply.github.com>
2026-08-03 15:16:07 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,13 @@ import polars as pl
 
 from dataclasses import dataclass
 from functools import wraps
+from pathlib import Path
 from time import sleep
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
+DB_PATH = DATA_DIR / "新刊通知リスト.xlsx"
 
 
 @dataclass
