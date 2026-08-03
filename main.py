ce8f1f8821a473a14919828ca30bae0afb1ff531
Shunki72 <144867611+Shunki72@users.noreply.github.com>
2026-08-03 15:18:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,11 @@
-import polars as pl
-
-from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
 from time import sleep
 
+from utils import *
 from bell_alert import *
 from google_calendar import *
 from line_messaging import *
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR / "data"
-DB_PATH = DATA_DIR / "新刊通知リスト.xlsx"
-
-
-@dataclass
-class BookRecord:
-    alert_id: int       # アラートID
-    category: str       # カテゴリー: 漫画 or ラノベ or 電子書籍
-    title: str          # タイトル
-    calendar_title: str # カレンダー登録用タイトル
-    volume: int         # 最新巻
-    release_date: str   # 発売日
-
-
-def load_db() -> list[BookRecord]:
-    table = pl.read_excel(DB_PATH)
-
-    book_db = []
-
-    for row in table.iter_rows(named=True):
-        book = BookRecord(
-            alert_id = row["アラートID"],
-            category = row["カテゴリー"],
-            title = row["タイトル"],
-            calendar_title = row["カレンダー用タイトル"],
-            volume = row["最新巻"],
-            release_date = row["発売日"]
-        )
-
-        book_db.append(book)
-
-    return book_db
-
-
-def save_db(book_db: list[BookRecord]):
-    rows = []
-
-    for book in book_db:
-        rows.append({
-            "アラートID": book.alert_id,
-            "カテゴリー": book.category,
-            "タイトル": book.title,
-            "カレンダー用タイトル": book.calendar_title,
-            "最新巻": book.volume,
-            "発売日": book.release_date
-        })
-
-    table = pl.DataFrame(rows).unique().sort("タイトル", "カテゴリー")
-    table.write_excel(DB_PATH)
 
 
 def output_log(text: str) -> None:
